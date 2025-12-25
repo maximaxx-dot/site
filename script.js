1d0efa009f3da2bebe5e3d1f2311159c5f1b75e6
unknown <max.ritale@gmail.com>
2025-12-25 17:49:50 +0100

    fff

---
+++ script.js
@@ -0,0 +1,51 @@
+function script (){
+        
+            const main = document.createElement('main');
+            const h2 = document.createElement('h2');
+            const p1 = document.createElement('p');
+            const p2 = document.createElement('p');
+            const p3 = document.createElement('p');
+            const p4 = document.createElement('p');
+            const p5 = document.createElement('p');
+            const p6 = document.createElement('p');
+            const p7 = document.createElement('p');
+            const h3 = document.createElement('h3');
+
+            const h2text = document.createTextNode('Jasmine');
+            const p1text = document.createTextNode("Je profite de cet éhange pour t'adresser un dernier mot, un mot qui j'espère résonnera en toi.");
+            const p2text = document.createTextNode("Pour commencer saches que je t'ai toujours aimé. Saches que je suis triste, triste de devoir tourner notre page. Cette tristesse je la tient de l'éspoir, l'éspoir que nous partagioons, avant que les doutes et les peurs ne s'installent. L'espoir d'avoir enfin trouvé cette personne de spéciale et au vu de notre entente je sais qu'on s'était trouvé.");
+            const p3text = document.createTextNode("J'aimerais que tu saches que j'aurais tellement voulu que ça s'arrange et qu'on puisse se retrouver, mais voilà aujourd'hui je dois faire face à ce choix, me renier et accepter les mots qui ont été les tiens ou choisir de ne pas accepter et me choisir moi. Et désolé pour nous mais je me choisis je refuse d'être traiter ainsi. Et le pire dans tout ça, c'est que sans ces mots je t'aurait dit oui, oui retrouvons nous et continuons ensemble.");
+            const p4text = document.createTextNode("J'aimerais que tu saches que malgré ces mots je n'ai jamais été en colère contre toi, que malgré tout je continue à t'aimer et à te respecter, car je sais que c'est tes peurs qui parlent. Je sais qu'au fond de toi tu ne le penses pas, je sais que c'est ton mécanisme de défense. C'est pour cela que je ne t'en veux pas.");
+            const p5text = document.createTextNode("Saches que lorsque je me perd dans mes pensés et que nos souvenirs me reviennent je suis hereux, hereux d'avoir été là, à cet instant avec toi. Que peut importe la situation je suis hereux d'avoir fait ce chemin avec toi. Ce sourire je le garderai et cela même s'il est accompagné d'un pincement dans mon coeur.");
+            const p6text = document.createTextNode("Pour terminer, j'aimerais que tu saches que je te souhaite d'être heureuse, je souhaite qu tu puisses enfin t'aimer à ta juste valeur et que tu puisses surmonter tes peurs et tes pensées qui te poussent à croire une fausse réalité.");
+            const p7text = document.createTextNode("Prends soin de toi ma pomme de la Terre, j'éspère que nous pourrons nous retrouver un jour. Je t'aime.");
+            const h3text = document.createTextNode('Maxime');
+
+            h2.appendChild(h2text);
+            p1.appendChild(p1text);
+            p2.appendChild(p2text);
+            p3.appendChild(p3text);
+            p4.appendChild(p4text);
+            p5.appendChild(p5text);
+            p6.appendChild(p6text);
+            p7.appendChild(p7text);
+            h3.appendChild(h3text);
+
+
+            main.appendChild(h2);
+            main.appendChild(p1);
+            main.appendChild(p2);
+            main.appendChild(p3);
+            main.appendChild(p4);
+            main.appendChild(p5);
+            main.appendChild(p6);
+            main.appendChild(p7);
+            main.appendChild(h3);
+
+            document.body.appendChild(main);
+        }
+
+
+
+
+  
